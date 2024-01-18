c73361e50907492fb5ba7b72337887dc753f142d
Jay <u7644495@anu.edu.au>
2024-01-18 17:22:01 +1100

    Add file open functionality to menu bar

--- lsq.py
+++ lsq.py
@@ -14,26 +14,37 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self): 
         super(MainWindow, self).__init__()
         self.setWindowTitle("Least-Squares calculator")
-        self.setGeometry(50, 50, 500, 300)
+        self.setFixedSize(500, 300)
         self.show()
         self.initialplot()
+        menu = MenuBar()
+        self.setMenuBar(menu)
 
     def initialplot(self):
         fig, ax = plt.subplots()
         self.setCentralWidget(FigureCanvas(fig))
 
-    def fileSelector(self):
-        btn = QtWidgets.QPushButton("Select file", self)
+class MenuBar(QtWidgets.QMenuBar):
+
+    def __init__(self):
+        super(MenuBar, self).__init__()
+        self.fileMenu = self.addMenu("File")
+        self.actOpen = self.fileMenu.addAction("Open")
+        self.actOpen.triggered.connect(self.fileOpen)
+
+    def fileOpen(self):
+        self.fileName = QtWidgets.QFileDialog.getOpenFileName(
+            self, "Open File", "", "Comma separated values (*.csv);;Excel File (*.xlsx *.xls)")
+        
         
 
 
 window = MainWindow()
-
-#helloMsg = QtWidgets.QLabel("<h1>Hello, World!</h1>")
-#mainLayout.addWidget(helloMsg)
 sys.exit(app.exec())
 
 fig, ax = plt.subplots()  # Create a figure containing a single axes.
 ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
 
 #plt.show()
+
+# Make work for more than 2 dimensional datasets
